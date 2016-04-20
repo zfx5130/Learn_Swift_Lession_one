206fbc2a19e71d3b3318456fd77fa215cb4f8212
thomas <dui1cuo@126.com>
2016-04-21 01:16:48 +0800

    [Add]add lesson message

--- Swift_Lession_one/Swift_Lession_one/ViewController.swift
+++ Swift_Lession_one/Swift_Lession_one/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel!
+    
+    //var 变量  let 常量
+    var displayZero: Bool = false;
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle! //这里!类似强制转换的意思，加!类型有optional到对应的string类型，
+        print("digit", digit) //打印方式， swift中一般不用写分号，在语句结尾。
+        if displayZero {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            displayZero = true  
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
